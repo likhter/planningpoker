1ab8f93553d00530a357561fd86e1d7eaa7504b1
Konstantin Likhter <konstantin@likhter.ru>
2017-11-17 21:58:24 -0800

    minor css improvements + github ribbon

--- pub/poker.js
+++ pub/poker.js
@@ -56,7 +56,7 @@ var _vm = function() {
     };
 
     this.prepareSocket = function(callback) {
-        this.socket = io.connect('http://tupi.ca:7654/');
+        this.socket = io.connect('http://pp.likhter.com:80/');
         this.socket.on('connect', function() {
             callback && callback();
         });
